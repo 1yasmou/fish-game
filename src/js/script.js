1792fae4e7f35422e4195d7d5485253f8f4b77fc
Yassine Moussaif <yassine.moussaif1234@gmail.com>
2024-04-24 11:04:47 +0200

    "v0tjrs"

--- src/js/script.js
+++ src/js/script.js
@@ -7,22 +7,21 @@
         const startTitle = document.querySelector("#start-title");
         const startBtn = document.querySelector("#start-btn");
         const scoreDisplay = document.querySelector('#score-display');
-
         const myAudio=document.querySelector("#myAudio");
 
       
         
-        //const myAudio = document.getElementById("myAudio");
+    //const myAudio = document.getElementById("myAudio");
 
     // myAudio.play();
 
     //TO SHOW!!
-         let fishIntervals = []; // Stock tous les intervals pour les poissons
+         let fishIntervals = []; 
          let moveInterval;
          let createFishInterval;
          let score = 0;
          let currentScore = 0;
-        let timer = 5;
+        let timer = 10;
         function updateTimerDisplay() {
         document.getElementById('timer').innerText = timer;
         }
@@ -31,7 +30,7 @@
             document.querySelector('#endgame-popup').style.display = 'none';
             score = 0;
     currentScore = 0;
-    timer = 5; 
+    timer = 10; 
     document.querySelector('#score-display').innerText = "Score: " + score;
     updateTimerDisplay();
 
@@ -104,11 +103,11 @@
            //console.log(mousePosition.x, mousePosition.y);
         }
 
-    /* j'appelle un eventlistener dans ma zone de click que j'ai appelé clickzone, le fait de bouger
-    la souris (mousemove) permet d'appeler la fonction monCurseur, à chaque fois que l'evenement mouse move
-    est détécté j'envoie les positions ou les coordonnées horizontals et verticals de la position de la souris
-    les x, et y de la souris sont ensuite affecté à mon element monViseur et met à jour les propriété left & right 
-    en ajoutant au chiffre récupéré les pixels*/
+                                                                                                                                                                                                                                        /* j'appelle un eventlistener dans ma zone de click que j'ai appelé clickzone, le fait de bouger
+                                                                                                                                                                                                                                        la souris (mousemove) permet d'appeler la fonction monCurseur, à chaque fois que l'evenement mouse move
+                                                                                                                                                                                                                                        est détécté j'envoie les positions ou les coordonnées horizontals et verticals de la position de la souris
+                                                                                                                                                                                                                                        les x, et y de la souris sont ensuite affecté à mon element monViseur et met à jour les propriété left & right 
+                                                                                                                                                                                                                                        en ajoutant au chiffre récupéré les pixels*/
 
 
     /*
@@ -180,7 +179,7 @@
         setPos(fish);
         fish.addEventListener("click", catchFish);
 
-        /*
+        
         setTimeout(function() {
             //remove
                 if (!fish.classList.contains("caught")){
@@ -192,13 +191,13 @@
                 }
                 }, 250); //frequency fish disappear
             }, 500); //frequency fish created
-            */
+            
     }
 
-    /*la fonction fish permet de créer un div contenant l'element fish chaque demi-second. et je lui affecte une position
-    aléatoire grace à la fonction setPos qu'on verra plus en détails par la suite.
-    si l'utilisateur arrive à capturer le poisson la classe caught est ajouté à puis je fais disparaitre le poisson
-    si non le poisson reste un petit moment avec de disparaitre sans ajouter la classe caught"*/
+                                                                                                                                                                        /*la fonction fish permet de créer un div contenant l'element fish chaque demi-second. et je lui affecte une position
+                                                                                                                                                                        aléatoire grace à la fonction setPos qu'on verra plus en détails par la suite.
+                                                                                                                                                                        si l'utilisateur arrive à capturer le poisson la classe caught est ajouté à puis je fais disparaitre le poisson
+                                                                                                                                                                        si non le poisson reste un petit moment avec de disparaitre sans ajouter la classe caught"*/
 
 
     function setPos(item) {
@@ -219,13 +218,13 @@
             }
         */
 
-        /* cette fonction permet de positionner le poisson dans ma zone de clique d'une facon aléatoire
-        un chiffre entre 0 et 1 est choisi puis multiplié par 2, si le chiffre et paire je mets le poisson 
-        dans le coté gauche de mon ecran si non je le mets à droite.
-        prenant l'ex. de celui d'à gauche. je recalcule ma leftposition en prenant que les dimension du coté gauche
-        (d'où le /2) et je rajoute la position du poisson par 5 px :
-        +5px pour le gauche vers la droite 
-        et -5px pour la droite vers la gauche. */
+                                                                                                                                                                                                                                    /* cette fonction permet de positionner le poisson dans ma zone de clique d'une facon aléatoire
+                                                                                                                                                                                                                                    un chiffre entre 0 et 1 est choisi puis multiplié par 2, si le chiffre et paire je mets le poisson 
+                                                                                                                                                                                                                                    dans le coté gauche de mon ecran si non je le mets à droite.
+                                                                                                                                                                                                                                    prenant l'ex. de celui d'à gauche. je recalcule ma leftposition en prenant que les dimension du coté gauche
+                                                                                                                                                                                                                                    (d'où le /2) et je rajoute la position du poisson par 5 px :
+                                                                                                                                                                                                                                    +5px pour le gauche vers la droite 
+                                                                                                                                                                                                                                    et -5px pour la droite vers la gauche. */
 
 
             //right side
@@ -253,6 +252,10 @@
                         this.parentNode.insertBefore(catchText,this);
                         catchText.style.left = this.style.left;
                         catchText.style.top = this.style.top;
+
+                        let topValue = parseInt(this.style.top, 10); 
+                        catchText.style.top = `${topValue - 50}px`;
+
                 //       if (!this.classList.contains("caught")){
                 //         this.classList.add("caught");
                                 if (type.contains("fish")) {
@@ -275,29 +278,40 @@
         }
 
         function endGame() {
-            // Affiche le score final
+     
             clearInterval(moveInterval); 
             clearInterval(createFishInterval);
             fishIntervals.forEach((id) => clearInterval(id));
             fishIntervals = [];
-            document.querySelector('#final-score').innerText = "Votre score est : " + currentScore;
-            // Affiche le popup
-            document.querySelector('#endgame-popup').style.display = 'flex';
             document.querySelectorAll('#click-zone .fish').forEach(fish => fish.remove());
+            document.querySelector('#final-score').innerText = "Votre score est : " + currentScore;
+            document.querySelector('#endgame-popup').style.display = 'flex';
+          
         }
 
 
-   
-        
+            };
+            
+                                                                                                                                                                                                                                                                                        /* l'event listener que j'ai ajouté lors de la création du poisson check s'il y a un clique
+                                                                                                                                                                                                                                                                                        et fait appel à la fonction catchfish , on check si le type est bien un poisson en verifiant si l'element
+                                                                                                                                                                                                                                                                                        cliqué contient la classe "fish" (peut être supprimé) , si le poisson cliqué ne contient pas la class caught on la rajoute
+                                                                                                                                                                                                                                                                                        en incremente le score et on l'affiche sur mon score display.*/
+
+                                                                                                                                                                                                                                                                                        /* un text a été ajouté juste à coté de l'endroit du catch pour montrer que la capture a été réalisée
+                                                                                                                                                                                                                                                                                        le text se delete après moins d'une seconde (settimeout) */
 
 
-    }; /* l'event listener que j'ai ajouté lors de la création du poisson check s'il y a un clique
-    et fait appel à la fonction catchfish , on check si le type est bien un poisson en verifiant si l'element
-    cliqué contient la classe "fish" (peut être supprimé) , si le poisson cliqué ne contient pas la class caught on la rajoute
-    en incremente le score et on l'affiche sur mon score display.*/
 
-    /* un text a été ajouté juste à coté de l'endroit du catch pour montrer que la capture a été réalisée
-    le text se delete après moins d'une seconde (settimeout) */
+                                                                                                                                                                                                                                                                                        /******************************* */
+
+
+
+                                                                                                                                                                                                                                                                                        /*endgame() dans ce bout de code, je clear ou nettoie tous les intervals créés soit pour de nouveau poisson ou pour le
+                                                                                                                                                                                                                                                                                            changment de position des poissons (pour ne pas consommer le cpu et la mémoire), je reinitialise mon tableau (array) d'interval
+                                                                                                                                                                                                                                                                                            je nettoie du DOM tous les elements ayant une classe fish et qui sont child direct de la clickzone à l'aide de la 
+                                                                                                                                                                                                                                                                                            fct remove();
+                                                                                                                                                                                                                                                                                            puis j'affiche le score final et un bouton restart pour redémarrer le jeu.*/
+
 
     document.addEventListener('DOMContentLoaded', function() {
         // Supposons que vous avez déjà votre élément audio dans votre HTML
